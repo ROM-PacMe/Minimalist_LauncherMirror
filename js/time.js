0a8cb8ea4d8c3186c3b2ed4699e48d040567fa42
Rafael PacMe <rafy.pachecomendez@gmail.com>
2019-01-17 22:31:30 -0300

    Update locale time complement with moments.js

--- js/time.js
+++ js/time.js
@@ -2,8 +2,11 @@ var thehours = new Date().getHours();
 
 //Moment.js is being used to display a live time feed
 
+// uso el idioma en español
+moment.locale('es');
+
 function update_date() {
   $('.time').html(moment().format('LT'));
-	$('.date').html(moment().format('dddd DD-MM YYYY'));
+	$('.date').html(moment().format('LL'));
 }
 setInterval(update_date, 1000);
